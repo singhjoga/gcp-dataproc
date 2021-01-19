09e071b55a167c101df4202fe6e96404181f7faa
JogaSingh <joga.singh-extern@deutschebahn.com>
2021-01-19 13:08:09 +0100

    test

--- gke-simple/gke-simple.tf
+++ gke-simple/gke-simple.tf
@@ -20,7 +20,7 @@ provider "google" {
 
 resource "google_container_cluster" "primary" {
   name     = "my-gke-cluster"
-  location = "${local.zone}"
+  location = local.zone
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
