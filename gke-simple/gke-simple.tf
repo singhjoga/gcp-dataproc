25db7d75652c07c7b0a1c5dca7b8e398db293443
JogaSingh <joga.singh-extern@deutschebahn.com>
2021-01-19 13:10:31 +0100

    test

--- gke-simple/gke-simple.tf
+++ gke-simple/gke-simple.tf
@@ -32,7 +32,7 @@ resource "google_container_cluster" "primary" {
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = "my-node-pool"
-  location   = "${local.zone}"
+  location   = local.zone
   cluster    = google_container_cluster.primary.name
   node_count = 2
 
