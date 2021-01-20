b089eb54e56685c10f5ddb187cfbd2e8f423b6f6
JogaSingh <joga.singh-extern@deutschebahn.com>
2021-01-20 09:31:00 +0100

    dddd

--- org-structure/org-structure.tf
+++ org-structure/org-structure.tf
@@ -41,6 +41,7 @@ resource "google_project" "test-vpc" {
   project_id = "test-vpc"
   auto_create_network = false
   folder_id  = google_folder.non-prod-shared.name
+  org_id     = "422558716844"
 #  billing_account = "01EFE4-BA1C6D-9714BD"
 }
 /*
