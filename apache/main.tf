83ae39ec94910e297a970cd4b1aed0c9b4151112
JogaSingh <joga.singh-extern@deutschebahn.com>
2021-01-26 13:38:48 +0100

    ss

--- apache/main.tf
+++ apache/main.tf
@@ -32,7 +32,7 @@ resource "google_compute_instance" "instance_with_ip" {
   network_interface {
     network = "default"
     access_config {
-      nat_ip = google_compute_address.static-ip.address
+      nat_ip = google_compute_address.ip_address.address
     }
   }
 }
