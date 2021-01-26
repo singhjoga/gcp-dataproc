ae321ba7045be7c2ce4d51fc62b0623156f2bd1e
JogaSingh <joga.singh-extern@deutschebahn.com>
2021-01-26 13:01:51 +0100

    dd

---
+++ apache/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_version = "~> 0.13"
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "~> 3.53.0"
+    }
+  }
+}
+locals {
+  region = "europe-west6"
+  zone = "europe-west6-a"
+  project_id="gke-learning-4caf"
+  host_project_id="vpc-nonprod-4caf"
+}
+
+provider "google" {
+	project	= local.project_id
+	region	= local.region
+	zone	= local.zone
+}
+data "google_compute_address" "ip_address" {
+  name = "static-ip"
+}
+
+resource "google_compute_instance" "instance_with_ip" {
+  name         = "apache"
+  machine_type = "f1-micro"
+  zone         = "europe-west6-a"
+
+
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.static-ip.address
+    }
+  }
+}
